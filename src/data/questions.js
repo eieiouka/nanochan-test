7b982f38b0c049a8577f2a9a263986fa15271494
eieiouka <eieiyukkuri@gmail.com>
2026-05-22 01:47:06 +0900

    j

--- src/data/questions.js
+++ src/data/questions.js
@@ -2,14 +2,12 @@ export const questions = [
   {
     id: 1,
     title: "第1問",
-    layout: "dice-square",
+    layout: "text-only",
     story:
       "桜羽エマはサイコロを使って、正方形を作ろうとしています。",
     question:
-      "サイコロが25個あります。一辺のサイコロはいくつ？",
+      "サイコロが25個あるとき、一辺のサイコロはいくつ？",
     answer: 5,
-    diceCount: 16,
-    gridSize: 4,
     answerLabel: "一辺",
     answerUnit: "個",
   },
