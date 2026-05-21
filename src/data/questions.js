bc9e1703d783189585a439e173eb54bb56f9cca5
eieiouka <eieiyukkuri@gmail.com>
2026-05-22 01:50:36 +0900

    h

--- src/data/questions.js
+++ src/data/questions.js
@@ -6,8 +6,20 @@ export const questions = [
     story:
       "桜羽エマはサイコロを使って、正方形を作ろうとしています。",
     question:
-      "サイコロが25個あるとき、一辺のサイコロはいくつ？",
-    answer: 5,
+      "一辺のサイコロが7個のとき、サイコロの合計数は？",
+    answer: 49,
+    answerLabel: "合計",
+    answerUnit: "個",
+  },
+  {
+    id: 2,
+    title: "第2問",
+    layout: "text-only",
+    story:
+      "桜羽エマはサイコロを使って、正方形を作ろうとしています。",
+    question:
+      "サイコロが169個あるとき、一辺のサイコロはいくつ？",
+    answer: 13,
     answerLabel: "一辺",
     answerUnit: "個",
   },
